38e5c9e6234be3f0739eec9b0a56578ed16c0ffa
Ben Amroota <79290187+Ben820@users.noreply.github.com>
2022-01-25 16:28:45 +0000

    Add files via upload

--- Automated fitting Lorentz Continuum.py
+++ Automated fitting Lorentz Continuum.py
@@ -21,6 +21,7 @@ import lmfit as lm
 import pylab
 import glob
 import pandas as pd 
+from collections import Counter
 #%%
 """ Part 1: Load data
 
@@ -29,7 +30,7 @@ Notes: data - MWD spectrum
        flux - y-values
 """
 #load data and sort into appropriate variables
-filename = "DESI_WDJ115418.15+011711.55_bin0p2.dat"
+filename = "DESI_WDJ205233.51-001610.73_bin0p2.dat"
 data = np.genfromtxt(f'{filename}', delimiter=' ')
 
 wavelength = data[:,0]
@@ -56,14 +57,14 @@ Notes: start/start_Ha - beginning of cut
 begin/ finish define the whole region including the triplet feature 
 startx/endx define the specific region to be cut out (the absorption feature) """
 
-begin = 5530
-finish = 7500
-start1 = 6070
-end1 = 6270
-start2 = 6400
-end2 = 6600
-start3 = 6775
-end3 = 7000
+begin = 5200
+finish = 8000
+start1 = 6304
+end1 = 6863
+start2 = 6865
+end2 = 6867
+start3 = 6869
+end3 = 6871
 
 
 start_Ha = int(np.where(wavelength == min(wavelength, key=lambda x:abs(x-begin)))[0])
@@ -131,12 +132,12 @@ norm_spectra = masked_Ha_flux/Continuum
 #plt.plot(masked_Ha_reg, Continuum)
 #plt.show()
 ##%%
-plt.figure()
-plt.plot(masked_Ha_reg, norm_spectra, label = f"{filename}") #plot the normalised spectrum
-plt.xlabel("Wavelength $\lambda$, $[\AA]$" , size = "15")
-plt.ylabel("Normalised Flux", size = "15")
-plt.legend()
-plt.show()
+#plt.figure()
+#plt.plot(masked_Ha_reg, norm_spectra, label = f"{filename}") #plot the normalised spectrum
+#plt.xlabel("Wavelength $\lambda$, $[\AA]$" , size = "15")
+#plt.ylabel("Normalised Flux", size = "15")
+#plt.legend()
+#plt.show()
 ##%%
 """ Triplet Lorentzian Profile 
 
@@ -181,7 +182,7 @@ Res_list = []
 B_list = []
 lambda0_list = []
 
-rangeBval = np.arange(0,50.5,0.5)
+rangeBval = np.arange(0,25,0.1)
 for i in rangeBval:
     p0 = np.array([6562.8, i, 0.8, 10, 0.8, 10])
     
@@ -254,9 +255,26 @@ if Res_list[index3] >= Res_list[index3]:
 if Res_list[index3] >= Res_list2[index3]:
     B_list[index3] = B_list2[index3]
 
+
 ##%%
 # The determined B value is BACK INTO curvefit to re-estimate the final parameters of the fit 
 # NOTE THIS IS STILL ONLY THE SECOND CURVEFIT - NOTE USE OF B_list NOT! B_list2
+popt_3lorentz, cov_3lorentz = opt.curve_fit(_3Lorentzian, xp_triplet, yp_triplet, \
+                                            p0=[6562.8, B_list[index3], 0.8, 10, 0.8, 10], \
+                                            sigma = err_triplet)#, \
+                                            #bounds = ((-np.inf, 0, -np.inf, -np.inf, -np.inf, -np.inf),(np.inf, np.inf, np.inf, np.inf, np.inf, np.inf)))
+
+# In the (1 instance so far) case that the lowest residuals outright does not conform to the best fit 
+# This is different to the reason for B_list2 and the secod curvefit; the second curvefit is used when
+# the first curvefit does not find the correct B, so finer precision is required
+# this case is when the correct B value IS identified, but is not selected because another (wrong)
+# B value has lower residuals 
+
+print("B = ", popt_3lorentz[1], 'pm', np.sqrt(np.diag(cov_3lorentz))[1])
+
+if np.around(np.array([popt_3lorentz[1]]), 2) != Counter(np.around(np.array(B_list),4)).most_common(1)[0][0]:
+    B_list[index3] = Counter(np.around(np.array(B_list),4)).most_common(1)[0][0]
+
 popt_3lorentz, cov_3lorentz = opt.curve_fit(_3Lorentzian, xp_triplet, yp_triplet, \
                                             p0=[6562.8, B_list[index3], 0.8, 10, 0.8, 10], \
                                             sigma = err_triplet)#, \
